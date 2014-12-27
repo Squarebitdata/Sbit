ec67c9ce28c82265c4c776d2af3e7a3a5305e0a9
Squarebits <Squarebitdata@gmail.com>
2014-12-27 13:23:21 -0500

    Sbits
    Squarebits

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SquareBit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SquareBitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
